25eedcb1c76a95e5d0b53f6b4ae7d6c295362893
Miguel Contreras <migueldevelop@gmail.com>
2016-12-17 01:25:18 -0400

    Agregada clase NewFileDialog y corregido error en Align.left

--- core/src/guerra/aeronaves/FileChooser.java
+++ core/src/guerra/aeronaves/FileChooser.java
@@ -22,6 +22,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldListener;
 import com.badlogic.gdx.scenes.scene2d.utils.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
 
 public class FileChooser extends Dialog {
